faaaf7357b9a35083fb9e67a8dba405305d71574
im-saumya-gupta <saumyagupt25@gmail.com>
2024-02-05 14:15:21 +0530

    chage host

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: ["http://localhost:3000","https://chatnest.netlify.app"],
+    origin: ["http://localhost:3000","https://chat-app-backend-jbiv.onrender.com"],
     // credentials: true,
   },
 });
