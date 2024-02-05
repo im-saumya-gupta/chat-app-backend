8483937059226f7c86ed6b238cad7ae705860339
im-saumya-gupta <saumyagupt25@gmail.com>
2024-02-05 19:25:00 +0530

    deployment test

--- backend/server.js
+++ backend/server.js
@@ -21,20 +21,24 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// ------------------ Deployment-------------------
+// --------------------------deployment------------------------------
+
 const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === "production")
-{
+
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
-  });
-}else{
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
   app.get("/", (req, res) => {
-  res.send("API Running!");
-});
+    res.send("API is running..");
+  });
 }
-// ------------------ Deployment-------------------
+
+// --------------------------deployment------------------------------
+
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
